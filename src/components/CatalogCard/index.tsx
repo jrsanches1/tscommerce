d9099b4d724334df99c2112bf49020dcf2ad7b19
jrsanches1 <sanchesjoseruy@gmail.com>
2025-04-25 11:28:50 -0300

    Catalog + Buttons created

---
+++ src/components/CatalogCard/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import compImg from '../../assets/comp.png';
+
+export default function CatalogCard() {
+
+    return (
+        <div className="dsc-card">
+            <div className="dsc-catalog-card-top dsc-line-bottom">
+                <img src={compImg} alt="Computer" />
+            </div>
+            <div className="dsc-catalog-card-bottom">
+                <h3>R$ 5000,00</h3>
+                <h4>
+                    Computador Gamer XT
+                </h4>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function ProductCategory() {
+
+    return (
+        <div className="dsc-category">
+            Eletrônicos
+        </div>
+
+    );
+
+}
