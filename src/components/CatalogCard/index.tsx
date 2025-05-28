5d917d486d89f6d8866975bf21474cb7ab6534d5
jrsanches1 <sanchesjoseruy@gmail.com>
2025-05-28 18:48:05 -0300

    Adding React Router Dom

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,17 +1,22 @@
 import './styles.css';
-import compImg from '../../assets/comp.png';
+import { ProductDTO } from '../../models/product';
 
-export default function CatalogCard() {
+
+type Props = {
+    product: ProductDTO;
+}
+
+export default function CatalogCard({ product }: Props) {
 
     return (
         <div className="dsc-card">
             <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={compImg} alt="Computer" />
+                <img src={product.imgUrl} alt={product.name} />
             </div>
             <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
+                <h3>R$ {product.price.toFixed(2)}</h3>
                 <h4>
-                    Computador Gamer XT
+                    {product.name}
                 </h4>
             </div>
         </div>
---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,46 @@
+import './styles.css';
+
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from '../../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV e muito bonita",
+    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletronicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
+
+export default function ProductDetails() {
+
+    return (
+
+
+        <main>
+            <section id="product-details-section" className="dsc-container">
+                <ProductDetailsCard product={product} />
+                <div className="dsc-btn-page-container">
+                    <ButtonPrimary text="Comprar" />
+                    <ButtonInverse text="Inicio" />
+                </div>
+            </section>
+        </main>
+
+    );
+}
