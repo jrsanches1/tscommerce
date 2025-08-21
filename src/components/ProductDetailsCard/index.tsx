775995533adba9098f99d0dc911170c23b9e9fa5
jrsanches1 <sanchesjoseruy@gmail.com>
2025-08-21 20:00:05 -0300

    error fixed

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -1,5 +1,4 @@
 import './styles.css';
-
 import ProductCategory from '../ProductCategory';
 import { ProductDTO } from '../../models/product';
 
@@ -7,9 +6,7 @@ type Props = {
     product: ProductDTO;
 }
 
-
 export default function ProductDetailsCard({ product }: Props) {
-
     return (
         <div className="dsc-card dsc-mb20">
             <div className="dsc-product-details-top dsc-line-bottom">
@@ -20,16 +17,13 @@ export default function ProductDetailsCard({ product }: Props) {
                 <h4>{product.name}</h4>
                 <p>{product.description} </p>
                 <div className="dsc-category-container">
-
                     {
                         product.categories.map(item => (
                             <ProductCategory key={item.id} name={item.name} />
                         ))
                     }
-
                 </div>
             </div>
         </div>
     );
-
 }
