62a2e7402b559109fe12e7f0678622f438e27d9f
jrsanches1 <sanchesjoseruy@gmail.com>
2025-08-23 18:28:45 -0300

    Searchbar onsearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import './styles.css';
 
+type Props = {
 
-export default function SearchBar() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    onSearch: Function;
+}
+
+export default function SearchBar({ onSearch }: Props) {
+
+    const [text, setText] = useState("");
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleChange(event: any) {
+        setText(event.target.value);
+    }
+
+    function handleResetClick() {
+        setText("");
+        onSearch(text);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        onSearch(text);
+    }
 
     return (
-        <form className="dsc-search-bar">
+        <form className="dsc-search-bar" onSubmit={handleSubmit}>
             <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">🗙</button>
+            <input
+                 value={text}
+                 type="text" 
+                 placeholder="Nome do produto" 
+                 onChange={handleChange}
+            />
+            <button onClick={handleResetClick}>🗙</button>
         </form>
     );
 }
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,33 +2,33 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
+import * as productService from '../../../services/product-service';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
-import * as productService from '../../../services/product-service';
-
-
 
 export default function Catalog() {
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
+    const [productName, setProductName] = useState("");
 
     useEffect(() => {
-
-        productService.findAll()
+        productService.findPageRequest(0, productName)
             .then(response => {
                 setProducts(response.data.content);
             });
-    }, []);
+    }, [productName]);
 
+    function handleSearch(searchText:string) {
+        setProductName(searchText)
+    }
 
     return (
         <main>
             <section id="catalog-section" className="dsc-container">
-                <SearchBar />
+                <SearchBar onSearch={handleSearch} />
 
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-
                     {
                         products.map(product => <CatalogCard key={product.id} product={product} />)
                     }
@@ -37,6 +37,5 @@ export default function Catalog() {
                 <ButtonNextPage />
             </section>
         </main>
-
     );
 }
